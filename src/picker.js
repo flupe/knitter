535a9a2931c685f6a0dabbaf718df455a3da67b8
flupe <lucas@escot.me>
2025-02-12 22:43:11 +0100

    fix color conversion

--- src/picker.js
+++ src/picker.js
@@ -1,29 +1,21 @@
-import {on, clamp} from "./prelude.js"
+import {on, clamp, lerp, HSLToRGB} from "./prelude.js"
 
-function hslToCol(h, s, l) {
-  let c = s * (1 - Math.abs(2 * l - 1))
-  let hueO60 = h / 60
-  let x = c * (1 - Math.abs(hueO60 % 2 - 1))
-  let m = l - c / 2
+let down       = false
+let SETTER     = null
+let saturation = 0
+let lightness  = 0
 
-  c += m
-  x += m
+// init --------------
+picker.tabIndex = -1
+  // picker can be focused
+picker_square.style.color = HSLToRGB(picker_hue.value, 1, 0.5)
+  // set square color to current hue
 
-  const index = Math.floor(hueO60) % 6;
-  const red   = [c, x, m, m, x, c][index] * 255 | 0
-  const green = [x, c, c, x, m, m][index] * 255 | 0
-  const blue  = [m, m, x, c, c, x][index] * 255 | 0
-
-  return `rgb(${red}, ${green}, ${blue})`
-}
-
-let down = false
-let SETTER = null
-
-
-on(picker_hue, "input", () =>
-  picker_square.style.color = hslToCol(picker_hue.value, 1, 0.5)
-)
+// whenever the hue is changed, update the square
+on(picker_hue, "input", () => {
+  picker_square.style.color = HSLToRGB(picker_hue.value, 1, 0.5)
+  update()
+})
 
 on(picker_square, "mousedown", e => {
   down = true
@@ -32,21 +24,32 @@ on(picker_square, "mousedown", e => {
 
 on(window, "mouseup", () => down = false)
 
+function update() {
+  if (SETTER) {
+    SETTER(HSLToRGB(
+      picker_hue.value,
+      saturation,
+      lightness,
+    ))
+  }
+}
+
 function moveMarker(mx, my){
   let rect = picker_square.getBoundingClientRect()
 
   let x = clamp(0, rect.width,  mx - rect.x)
   let y = clamp(0, rect.height, my - rect.y)
 
+  let dx = x       / rect.width
+  let dy = 1.0 - y / rect.height
+  saturation = x / rect.width
+  lightness  = lerp(lerp(0, 1.0, dy), lerp(0, 0.5, dy), dx)
+
   picker_marker.style.left = `${x}px`
   picker_marker.style.top  = `${y}px`
 
-  if (SETTER) {
-    SETTER(hslToCol(picker_hue.value, x / rect.width, (x + y) / (rect.width + rect.height)))
-  }
+  update()
 }
-
-picker.tabIndex = -1
 
 on(picker_square, "mousemove", e => {
   if (!down) return
--- src/prelude.js
+++ src/prelude.js
@@ -8,6 +8,7 @@ export const on = (t, e, f) =>
         : t.addEventListener(e, f)
 
 export const clamp = (a, b, x) => x < a ? a : x > b ? b : x
+export const lerp  = (a, b, x) => a + x * (b - a)
 
 // return a copy of a function bound to its parent object
 export const bind = (o, f) => o[f].bind(o)
@@ -24,9 +25,9 @@ export const stitchTable =
   , "loc"
   ]
 
-export const debounce = (delay, fn) => {
+export function debounce(delay, fn) {
   let timeout = null
-  let cb = () => {
+  function cb() {
     timeout = null
     fn()
   }
@@ -34,4 +35,24 @@ export const debounce = (delay, fn) => {
     if (timeout !== null) clearTimeout(timeout)
     timeout = setTimeout(cb, delay)
   }
+}
+
+// 0 ≤ h ≤ 360
+// 0 ≤ s ≤ 1
+// 0 ≤ l ≤ 1
+export function HSLToRGB(h, s, l) {
+  h = h / 60
+  let c = s * (1 - Math.abs(2 * l - 1))
+  let x = c * (1 - Math.abs(h % 2 - 1))
+  let m = l - c / 2
+
+  c += m
+  x += m
+  h = h % 6 | 0
+
+  const r = [c, x, m, m, x, c][h] * 255 | 0
+  const g = [x, c, c, x, m, m][h] * 255 | 0
+  const b = [m, m, x, c, c, x][h] * 255 | 0
+
+  return `rgb(${r}, ${g}, ${b})`
 }
